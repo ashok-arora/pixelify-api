f5f9734c42a3b21152a9ae40fe1fd50694d49570
Ashok Arora <arora8715@gmail.com>
2021-11-07 17:58:48 +0400

    / check 3

--- api/index.py
+++ api/index.py
@@ -1,20 +1,20 @@
-import base64
-import bcrypt
-import firebase_admin
-from firebase_admin import credentials, firestore
+# import base64
+# import bcrypt
+# import firebase_admin
+# from firebase_admin import credentials, firestore
 from flask import Flask, request
-from io import BytesIO
-import numpy as np
-import pickle
-import piexif
-from PIL import Image
-import random
-import uuid
-import os
+# from io import BytesIO
+# import numpy as np
+# import pickle
+# import piexif
+# from PIL import Image
+# import random
+# import uuid
+# import os
 
-from api.caesar import Caesar
+# from api.caesar import Caesar
 
-from cryptography.fernet import Fernet
+# from cryptography.fernet import Fernet
 
 # decrypt file using secret key
 # key = os.environ.get("PIXELIFY_KEY")
@@ -45,80 +45,80 @@ def ping():
     return "pong!", 200
 
 
-@app.route("/encrypt", methods=["POST"])
-def encrypt():
-    params = request.json
-    password = params['password']
-    image = params['image']
-    cipher = params['cipher']
-    # firestore client
-    db = firestore.client()
+# @app.route("/encrypt", methods=["POST"])
+# def encrypt():
+#     params = request.json
+#     password = params['password']
+#     image = params['image']
+#     cipher = params['cipher']
+#     # firestore client
+#     db = firestore.client()
 
-    # hash password using bcrypt
-    salt = bcrypt.gensalt(rounds=12)
-    password_bytes = str.encode(password)
-    hash = bcrypt.hashpw(password_bytes, salt)
+#     # hash password using bcrypt
+#     salt = bcrypt.gensalt(rounds=12)
+#     password_bytes = str.encode(password)
+#     hash = bcrypt.hashpw(password_bytes, salt)
 
-    # generate id, firestore doesn't support auto-increment id
-    id = str(uuid.uuid4())
+#     # generate id, firestore doesn't support auto-increment id
+#     id = str(uuid.uuid4())
 
-    # store hash and id in firestore
-    doc_ref = db.collection("hash-id").document(id)
-    doc_ref.set({"hash": hash, "id": id})
+#     # store hash and id in firestore
+#     doc_ref = db.collection("hash-id").document(id)
+#     doc_ref.set({"hash": hash, "id": id})
 
-    # generate key for cipher
-    key = random.randint(0, 255)
+#     # generate key for cipher
+#     key = random.randint(0, 255)
 
-    # store id, cipher and key in firestore
-    doc_ref = db.collection("id-cipher-key").document(id)
-    doc_ref.set({"id": id, "cipher": cipher, "key": key})
+#     # store id, cipher and key in firestore
+#     doc_ref = db.collection("id-cipher-key").document(id)
+#     doc_ref.set({"id": id, "cipher": cipher, "key": key})
 
-    # convert image to numpy array
-    image_head = image[:image.index(',')+1]
-    image = image[image.index(',')+1:]
-    base64_decoded = base64.b64decode(image)
-    img = Image.open(BytesIO(base64_decoded))
-    img = np.array(img)
+#     # convert image to numpy array
+#     image_head = image[:image.index(',')+1]
+#     image = image[image.index(',')+1:]
+#     base64_decoded = base64.b64decode(image)
+#     img = Image.open(BytesIO(base64_decoded))
+#     img = np.array(img)
 
-    # encrypt
-    cipher = Caesar(key=key)
-    cipher.encrypt(img)
+#     # encrypt
+#     cipher = Caesar(key=key)
+#     cipher.encrypt(img)
 
-    # adding id to metadata
-    data = pickle.dumps(id)
-    exif_ifd = {piexif.ExifIFD.MakerNote: data}
-    exif_dict = {"Exif": exif_ifd}
-    exif_dat = piexif.dump(exif_dict)
+#     # adding id to metadata
+#     data = pickle.dumps(id)
+#     exif_ifd = {piexif.ExifIFD.MakerNote: data}
+#     exif_dict = {"Exif": exif_ifd}
+#     exif_dat = piexif.dump(exif_dict)
 
-    # convert encrypted image to base64
-    pil_img = Image.fromarray(img)
-    buff = BytesIO()
-    pil_img.save(buff, format="JPEG", exif=exif_dat)
-    encrypted_image = base64.b64encode(buff.getvalue()).decode("utf-8")
-    encrypted_image = image_head+encrypted_image
+#     # convert encrypted image to base64
+#     pil_img = Image.fromarray(img)
+#     buff = BytesIO()
+#     pil_img.save(buff, format="JPEG", exif=exif_dat)
+#     encrypted_image = base64.b64encode(buff.getvalue()).decode("utf-8")
+#     encrypted_image = image_head+encrypted_image
 
-    return encrypted_image, 200
+#     return encrypted_image, 200
 
 
-@app.route("/decrypt", methods=["POST"])
-def decrypt():
-    params = request.json
-    password = params['password']
-    image = params['image']
-    cipher = params['cipher']
-    # hash password using bcrypt
+# @app.route("/decrypt", methods=["POST"])
+# def decrypt():
+#     params = request.json
+#     password = params['password']
+#     image = params['image']
+#     cipher = params['cipher']
+#     # hash password using bcrypt
 
-    # check if hash exists
+#     # check if hash exists
 
-    # if hash exists, extract id from image metadata
+#     # if hash exists, extract id from image metadata
 
-    # find corresponding cipher and key from firestore
+#     # find corresponding cipher and key from firestore
 
-    # decrypt
+#     # decrypt
 
-    # return decrypted base64 image
+#     # return decrypted base64 image
 
-    return "not hehe", 200
+#     return "not hehe", 200
 
 
 if __name__ == "__main__":
