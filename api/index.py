58c1f9ad7e5317511038ac6ce1a2b4b834686f6a
Aniket Sharma <aniketsharma00411@gmail.com>
2021-11-10 17:02:39 +0530

    Adds decryption

--- api/index.py
+++ api/index.py
@@ -17,35 +17,35 @@ from api.caesar import Caesar
 from cryptography.fernet import Fernet
 
 # decrypt file using secret key
-key = os.environ.get("PIXELIFY_KEY")
+key = os.environ.get('PIXELIFY_KEY')
 f = Fernet(key)
 
-with open("./api/serviceAccountKey.enc", "rb") as encrypted_file:
+with open('./api/serviceAccountKey.enc', 'rb') as encrypted_file:
     encrypted = encrypted_file.read()
 
 decrypted = f.decrypt(encrypted)
-with open("./api/serviceAccountKey.json", "wb") as decrypted_file:
+with open('./api/serviceAccountKey.json', 'wb') as decrypted_file:
     decrypted_file.write(decrypted)
 
 
 # Use a service account
-cred = credentials.Certificate("./api/serviceAccountKey.json")
+cred = credentials.Certificate('./api/serviceAccountKey.json')
 firebase_admin.initialize_app(cred)
 
 app = Flask(__name__)
 
 
-@app.route("/", methods=["GET"])
+@app.route('/', methods=['GET'])
 def index():
-    return "Pixelify backend API. Made using Flask and Python.", 200
+    return 'Pixelify backend API. Made using Flask and Python.', 200
 
 
-@app.route("/ping", methods=["GET"])
+@app.route('/ping', methods=['GET'])
 def ping():
-    return "pong!", 200
+    return 'pong!', 200
 
 
-@app.route("/encrypt", methods=["POST"])
+@app.route('/encrypt', methods=['POST'])
 def encrypt():
     params = request.json
     password = params['password']
@@ -63,15 +63,15 @@ def encrypt():
     id = str(uuid.uuid4())
 
     # store hash and id in firestore
-    doc_ref = db.collection("hash-id").document(id)
-    doc_ref.set({"hash": hash, "id": id})
+    doc_ref = db.collection('hash-id').document(id)
+    doc_ref.set({'hash': hash, 'id': id})
 
     # generate key for cipher
     key = random.randint(0, 255)
 
     # store id, cipher and key in firestore
-    doc_ref = db.collection("id-cipher-key").document(id)
-    doc_ref.set({"id": id, "cipher": cipher, "key": key})
+    doc_ref = db.collection('id-cipher-key').document(id)
+    doc_ref.set({'id': id, 'cipher': cipher, 'key': key})
 
     # convert image to numpy array
     image_head = image[:image.index(',')+1]
@@ -87,39 +87,55 @@ def encrypt():
     # adding id to metadata
     data = pickle.dumps(id)
     exif_ifd = {piexif.ExifIFD.MakerNote: data}
-    exif_dict = {"Exif": exif_ifd}
+    exif_dict = {'Exif': exif_ifd}
     exif_dat = piexif.dump(exif_dict)
 
     # convert encrypted image to base64
     pil_img = Image.fromarray(img)
     buff = BytesIO()
-    pil_img.save(buff, format="JPEG", exif=exif_dat)
-    encrypted_image = base64.b64encode(buff.getvalue()).decode("utf-8")
+    pil_img.save(buff, format='JPEG', exif=exif_dat)
+    encrypted_image = base64.b64encode(buff.getvalue()).decode('utf-8')
     encrypted_image = image_head+encrypted_image
 
     return encrypted_image, 200
 
 
-@app.route("/decrypt", methods=["POST"])
+@app.route('/decrypt', methods=['POST'])
 def decrypt():
     params = request.json
     password = params['password']
     image = params['image']
     cipher = params['cipher']
-    # hash password using bcrypt
 
-    # check if hash exists
+    # convert image
+    image_head = image[:image.index(',')+1]
+    image = image[image.index(',')+1:]
+    base64_decoded = base64.b64decode(image)
+    img = Image.open(BytesIO(base64_decoded))
+    img_mat = np.array(img)
 
-    # if hash exists, extract id from image metadata
+    # extract id
+    exif_dict = piexif.load(img.info['exif'])
+    id = pickle.loads(exif_dict['Exif'][piexif.ExifIFD.MakerNote])
 
-    # find corresponding cipher and key from firestore
+    # get password from firestore and verify
 
-    # decrypt
+    # get key from firestore
+    key = 1
 
-    # return decrypted base64 image
+    # decrypt image
+    cipher = Caesar(key=key)
+    cipher.decrypt(img_mat)
 
-    return "not hehe", 200
+    # convert encrypted image to base64
+    pil_img = Image.fromarray(img_mat)
+    buff = BytesIO()
+    pil_img.save(buff, format='JPEG')
+    decrypted_image = base64.b64encode(buff.getvalue()).decode('utf-8')
+    decrypted_image = image_head+decrypted_image
+
+    return decrypted_image, 200
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True, port=80)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True, port=80)
